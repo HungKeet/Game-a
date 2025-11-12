110e1d4a18171d1fb9a2615360f9f916de7e34b8
Nguyễn Huy Hùng <huyhung23072006@gmail.com>
2025-11-12 11:42:47 +0700

    Update app.config.server.ts

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,10 +3,21 @@ import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
+// 1. IMPORT "NO-OP" ANIMATIONS
+// Đây là bản sửa lỗi cho "@angular/animations/browser"
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes))
+    // 2. Sửa lỗi Vercel/Routes (giống như code của bạn)
+    provideServerRendering(withRoutes(serverRoutes)),
+    
+    // 3. SỬA LỖI ANIMATIONS
+    // Lệnh này sẽ ghi đè (override) lên provideAnimations() 
+    // của file app.config.ts, và sửa lỗi build animations
+    provideNoopAnimations()
   ]
 };
 
+// Gộp 2 file config lại
 export const config = mergeApplicationConfig(appConfig, serverConfig);
