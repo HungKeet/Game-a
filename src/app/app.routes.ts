c97bc6d2ccb1553ef012febf684dc0be45d60fb5
Nguyễn Huy Hùng <huyhung23072006@gmail.com>
2025-11-12 11:07:20 +0700

    Fix import path for DetailsComponent and update routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,21 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
-import { DetailsComponent } from '../app/components/details/details';
+import { DetailsComponent } from './components/details/details';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+    prerender: true
   },
   {
     path: 'search/:game-search',
     component: Home,
+    prerender: false
   },
   {
     path: 'details/:id',
     component: DetailsComponent,
+    prerender: false
   },
 ];
