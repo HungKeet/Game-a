b99545053b3f57ab9ad31715df7130d4882f2fcc
Keet <huyhung23072006@gmail.com>
2026-01-12 15:00:02 +0700

    ...

--- src/app/services/http.ts
+++ src/app/services/http.ts
@@ -1,5 +1,3 @@
-// src/app/services/http.service.ts
-
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment as env } from '../../environments/environment.prod';
@@ -11,20 +9,11 @@ import { Observable } from 'rxjs';
 })
 export class HttpService {
   constructor(private http: HttpClient) {}
-
-  /**
-   * Sửa hàm này: Thêm tham số 'page: number'
-   */
-  getGamelist(
-    ordering: string,
-    page: number, // <-- THÊM THAM SỐ NÀY
-    search?: string
-  ): Observable<APIResponse<Game>> {
-    // Cập nhật 'params'
+  getGamelist(ordering: string, page: number, search?: string): Observable<APIResponse<Game>> {
     let params = new HttpParams()
       .set('ordering', ordering)
-      .set('page', page.toString()) // <-- THÊM DÒNG NÀY
-      .set('page_size', '20'); // <-- Thêm dòng này để API luôn trả về 20
+      .set('page', page.toString())
+      .set('page_size', '20');
 
     if (search) {
       params = params.set('search', search);
@@ -34,10 +23,6 @@ export class HttpService {
       params: params,
     });
   }
-
-  /**
-   * Hàm này giữ nguyên
-   */
   getGameDetails(id: string): Observable<Game> {
     const params = new HttpParams();
     return this.http.get<Game>(`${env.BASE_URL}/games/${id}`, {
